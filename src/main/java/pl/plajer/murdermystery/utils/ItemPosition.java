c1fdfca070c55af15c507193b2203bd1217f8c2b
Plajer <worthplay.pt@onet.pl>
2018-12-26 12:36:27 +0100

    Murderer gold pickup fix

--- src/main/java/pl/plajer/murdermystery/utils/ItemPosition.java
+++ src/main/java/pl/plajer/murdermystery/utils/ItemPosition.java
@@ -13,6 +13,21 @@
  * along with Murder Mystery.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * Murder Mystery is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Murder Mystery is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Murder Mystery.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package pl.plajer.murdermystery.utils;
 
 import org.bukkit.entity.Player;
@@ -55,6 +70,7 @@ public enum ItemPosition {
     if (Role.isRole(Role.MURDERER, player)) {
       if (inv.getItem(itemPosition.getMurdererItemPosition()) != null) {
         inv.getItem(itemPosition.getMurdererItemPosition()).setAmount(inv.getItem(itemPosition.getMurdererItemPosition()).getAmount() + itemStack.getAmount());
+        return;
       }
       inv.setItem(itemPosition.getMurdererItemPosition(), itemStack);
     } else {
