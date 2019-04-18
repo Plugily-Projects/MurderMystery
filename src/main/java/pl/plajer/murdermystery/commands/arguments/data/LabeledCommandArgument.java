44a1071a8f44b619b829f1a0156877fd132796f3
Plajer <plajer.xyz@gmail.com>
2019-04-18 10:10:18 +0200

    Remove copyright notices, keep only one per file I did the whole thing manually, pls help

--- src/main/java/pl/plajer/murdermystery/commands/arguments/data/LabeledCommandArgument.java
+++ src/main/java/pl/plajer/murdermystery/commands/arguments/data/LabeledCommandArgument.java
@@ -1,6 +1,6 @@
 /*
  * MurderMystery - Find the murderer, kill him and survive!
- * Copyright (C) 2019  Plajer's Lair - maintained by Plajer and Tigerpanzer
+ * Copyright (C) 2019  Plajer's Lair - maintained by Plajer and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
