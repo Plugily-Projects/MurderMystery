c3c6518c623b9c2a076b7714b55146a6253fe03b
Fabian Mäkilä <44451855+FaberoM@users.noreply.github.com>
2020-08-02 22:37:25 +0300

    Replace Math.ceil() with a better function

--- src/main/java/plugily/projects/murdermystery/utils/Utils.java
+++ src/main/java/plugily/projects/murdermystery/utils/Utils.java
@@ -65,7 +65,8 @@ public class Utils {
    * @return serialized number
    */
   public static int serializeInt(Integer i) {
-    return (i % 9) == 0 ? i : (int) ((Math.ceil(i / 9) * 9) + 9);
+    if (i == 0) return 9; //The function bellow doesn't work if i == 0, so return 9 in case that happens.
+    return (i % 9) == 0 ? i : (i + 9 - 1) / 9 * 9;
   }
 
   /**
--- src/test/java/plugily/projects/murdermystery/utils/UtilsTest.java
+++ src/test/java/plugily/projects/murdermystery/utils/UtilsTest.java
@@ -31,6 +31,8 @@ public class UtilsTest {
   @Test
   public void serializeInt() {
     Assert.assertEquals(9, Utils.serializeInt(3));
+    Assert.assertEquals(9, Utils.serializeInt(9));
+    Assert.assertEquals(27, Utils.serializeInt(24));
     Assert.assertEquals(45, Utils.serializeInt(37));
     Assert.assertEquals(45, Utils.serializeInt(43));
   }
